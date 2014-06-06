3bf278661384c6f34bfeda21878e65f33491468f
RemideZ <remi.de.z@gmail.com>
2014-06-06 14:57:26 +0200

    tralala

--- processing_data/line.py
+++ processing_data/line.py
@@ -1,2 +1,32 @@
+from os import listdir
+
 print "Hello line!"
 
+base = "../data/original.serve/"
+outbase = "../data/cleaned.serve/"
+all_files = listdir(base)
+
+for name in all_files:
+	f = open(base+name, 'r')
+	out = open(outbase+name, 'w')
+	for line in f.readlines():
+		line = list(line)
+		while len(line)>0:
+			char = line.pop(0)
+			if char == "<":
+				line.pop(0)
+				line.pop(0)
+				break
+			if char == ":":
+				break
+		if ":" in "".join(line).split(" ")[0]:
+			while not line.pop(0) == ":":
+				pass
+		line = "".join(line)
+		for garbage in ["<s>", "</s>", "<p>", "</p>", "@"]:
+			line = line.replace(garbage, "")
+		while not line == line.replace("  ", ""):
+			line = line.replace("  ", "")
+		out.write(line)
+	f.close()
+	out.close()
--- processing_data/serve.py
+++ processing_data/serve.py
@@ -1,2 +1,19 @@
+from os import listdir
+
 print "Hello serve!"
+
+base = "../data/original.serve/"
+outbase = "../data/cleaned.serve/"
+all_files = listdir(base)
+
+for name in all_files:
+	f = open(base+name, 'r')
+	out = open(outbase+name, 'w')
+	for line in f.readlines():
+		line = line.split(" ")
+		line.pop(0)
+		line = "".join([ x + " " for x in line])
+		out.write(line)
+	f.close()
+	out.close()
 
