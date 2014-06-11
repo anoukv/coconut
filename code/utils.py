ea13e0238fb425bf687824cd1d0dd2cc71e274e5
anoukv <anouk.visser@student.uva.nl>
2014-06-11 15:21:24 +0200

    added load_vectors to utils

--- code/utils.py
+++ code/utils.py
@@ -9,3 +9,23 @@ def postag(word):
 
 def pos_is_revelant(pos):
 	return pos in set(['N', 'NP', 'VG', 'VD', 'ADJ', 'ADV'])
+
+# returns normalized word vectors for every word from 'filename'
+def load_vectors(filename):
+	def normalizeString(vec):
+		vec = [ float(x) for x in vec]
+		total = sqrt( sum([v**2 for v in vec]) )
+		new_vec = []
+		for v in vec:
+			new_vec.append(v/total)
+		return tuple(new_vec)
+	print "\tLoading projections..."
+	f = open(filename,'r')
+	f.readline()
+	content = [ filter( lambda x : not x in ["\n",""], l.replace("\n", "").split(" ")) for l in f.readlines() ]
+	content = [ (l[0], normalizeString(l[1:])) for l in content ]
+	content = filter(lambda x : not x[1] == None, content)
+	words = dict()
+	for (word, vector) in content:
+		words[word.lower()] = vector
+	return words
