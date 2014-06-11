934e4631ae18d431a04cbbb140d44257510b7ebe
anoukv <anouk.visser@student.uva.nl>
2014-06-11 11:40:12 +0200

    variance and mean of bins

--- inspecting_data/inspectionTools.py
+++ inspecting_data/inspectionTools.py
@@ -1,7 +1,18 @@
-import sys
+import sys, random
 from math import sqrt
 from collections import defaultdict
 from matplotlib import pyplot as plt
+
+def getMean(someListOfNumbers):
+	return sum(someListOfNumbers) / float(len(someListOfNumbers))
+
+def getVariance(someListOfNumbers):
+	mean = getMean(someListOfNumbers)
+	summationResult = 0
+	for elem in someListOfNumbers:
+		summationResult += (elem - mean)**2
+	return summationResult / float(len(someListOfNumbers))
+
 def load_vectors(filename):
 
 	def normalizeString(vec):
@@ -26,14 +37,14 @@ def load_vectors(filename):
 def getHistogramOfSimilarities(vectors, word):
 	wordRepresentation = vectors[word]
 	dim = len(wordRepresentation)
-	histogram = defaultdict(int)
+	histogram = defaultdict(set)
 	for wordvector in vectors:
 		if wordvector != word:
 			rep = vectors[wordvector]
 			sim = round(sum([wordRepresentation[i] * rep[i] for i in xrange(dim)])*10)/10
-			histogram[sim] += 1
+			histogram[sim].add(wordvector)
 	labels = [x / float(10) for x in range(-9, 10, 1)]
-	counts = [histogram[label] for label in labels]
+	counts = [len(histogram[label]) for label in labels]
 
 	x = range(len(labels))
 	plt.bar(x, counts, align='center', width=0.5)
@@ -42,6 +53,22 @@ def getHistogramOfSimilarities(vectors, word):
 	plt.ylim(0, ymax)
 	plt.show()
 
+	for bin in histogram:
+		words = list(histogram[bin])
+		if len(words) > 0:
+			similarities = []
+			for i in range(100):
+				word1 = random.choice(words)
+				word2 = random.choice(words)
+				if word1 != word2:
+					wordvec1 = vectors[word1]
+					wordvec2 = vectors[word2]
+					sim = sum([wordvec1[i] * wordvec2[i] for i in xrange(dim)])
+					print word1, word2, sim
+					similarities.append(sim)
+			if len(similarities) > 0:
+				print bin, getMean(similarities), getVariance(similarities)
+			print "\n --------------------------------------- \n"
 	return histogram
 
 	
@@ -54,4 +81,4 @@ if __name__ == "__main__":
 	filename = sys.argv[1]
 	vectors = load_vectors(filename)
 	print len(vectors), " words found in wordvector "
-	getHistogramOfSimilarities(vectors, 'line')
+	getHistogramOfSimilarities(vectors, 'apple')
