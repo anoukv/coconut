dfb68fcc754a292b142726d54b63fc4b9085e5c6
anoukv <anouk.visser@student.uva.nl>
2014-06-12 09:34:01 +0200

    moved things to utils

--- code/utils.py
+++ code/utils.py
@@ -29,3 +29,60 @@ def load_vectors(filename):
 	for (word, vector) in content:
 		words[word.lower()] = vector
 	return words
+
+# reads the SCWS task
+def read_task(filename):
+	task = dict()
+	f = open(filename, 'r')
+	lines = f.readlines()
+	for question in lines:
+		q = dict()
+		question = question.split('\t')
+		q['word1'] = question[1]
+		q['pos1'] = question[2]
+		q['word2'] = question[3]
+		q['pos2'] = question[4]
+		q['context1'] = question[5]
+		q['context2'] = question[6]
+		q['rating'] = question[7]
+		task[question[0]] = q
+	return task
+
+# returns Spearman's Rank Correlation
+def spearman(x, y):
+	# add identifiers to to lists
+	idX = [(i, x[i]) for i in xrange(len(x))]
+	idY = [(i, y[i]) for i in xrange(len(y))]
+
+	# sort the lists with identifier tuples based on the observed variable
+	idX = sorted(idX, key = lambda x: x[1])
+	idY = sorted(idY, key = lambda x: x[1])
+
+	# throw away the values
+	idX = map(lambda x: x[0], idX)
+	idY = map(lambda x: x[0], idY)
+
+	# get the ranking for the observed variables
+	xx = [idX.index(i) + 1 for i in xrange(len(idX))]
+	yy = [idY.index(i) + 1 for i in xrange(len(idY))]
+
+	# compute the mean for the rankings (meanX == meanY)
+	meanX = sum(xx) / float(len(xx))
+	meanY = sum(yy) / float(len(yy))
+
+	# iterate over all observed variable rankings and perform 
+	# computations
+	numerator = 0
+	denominator1 = 0
+	denominator2 = 0
+	
+	for i in xrange(len(xx)):
+		numerator += (xx[i] - meanX) * (yy[i] - meanY)
+		denominator1 += (xx[i] - meanX)**2
+		denominator2 += (yy[i] - meanY)**2
+	
+	# result!
+	denominator = sqrt(denominator1 * denominator2)
+	rho = numerator / float(denominator)
+	
+	return rho
