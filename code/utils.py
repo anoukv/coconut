c3b030cb34ac00918aa75660db0e18c4e7d50af7
RemideZ <remi.de.z@gmail.com>
2014-06-12 13:59:41 +0200

    utils repaired

--- code/utils.py
+++ code/utils.py
@@ -16,36 +16,6 @@ def pos_is_revelant(pos):
 def word_is_relevant(word):
 	return pos_is_revelant(postag(word))
 
-def read_vecs(limit=90000):
-	f1 = open("../vectors/vocabul50", 'r')
-	f2 = open("../vectors/vectors50", 'r')
-	lines = zip(f1.readlines(), f2.readlines())[:limit]
-	lines = filter(lambda x : word_is_relevant(x[0]), lines)
-	f1.close()
-	f2.close()
-	lines = [ (x.replace("\n", ""), map(lambda n : float(n), filter(lambda p : len(p) > 2, y.replace("\n","").split(" ")))) for (x,y) in lines ]
-	return lines
-
-# returns normalized word vectors for every word from 'filename'
-def load_vectors(filename):
-	def normalizeString(vec):
-		vec = [ float(x) for x in vec]
-		total = sqrt( sum([v**2 for v in vec]) )
-		new_vec = []
-		for v in vec:
-			new_vec.append(v/total)
-		return tuple(new_vec)
-	print "\tLoading projections..."
-	f = open(filename,'r')
-	f.readline()
-	content = [ filter( lambda x : not x in ["\n",""], l.replace("\n", "").split(" ")) for l in f.readlines() ]
-	content = [ (l[0], normalizeString(l[1:])) for l in content ]
-	content = filter(lambda x : not x[1] == None, content)
-	words = dict()
-	for (word, vector) in content:
-		words[word.lower()] = vector
-	return words
-
 # reads the SCWS task
 def load_task(filename):
 	task = dict()
@@ -139,3 +109,48 @@ def spearman2(x, y):
 # vec1 and vec2 should already be normalized!!
 def cosine_similarity(vec1, vec2):
 	return sum([vec1[i] * vec2[i] for i in xrange(len(vec1))])
+
+
+# returns normalized word vectors for every word from 'filename'
+def load_vectors(filename, limit=False, filterRelevant=True):
+	def normalizeString(vec):
+		vec = [ float(x) for x in vec]
+		total = sqrt( sum([v**2 for v in vec]) )
+		new_vec = []
+		for v in vec:
+			new_vec.append(v/total)
+		return tuple(new_vec)
+	f = open(filename,'r')
+	f.readline()
+	content = [ filter( lambda x : not x in ["\n",""], l.replace("\n", "").split(" ")) for l in f.readlines() ]
+	content = [ (l[0], normalizeString(l[1:])) for l in content ]
+	content = filter(lambda x : not x[1] == None, content)
+	if filterRelevant and not limit == False:
+		newC = []
+		while limit > 0:
+			item = content.pop(0)
+			if word_is_relevant(item[0]):
+				newC.append(item)
+				limit -= 1
+		content = newC
+	else:
+		if filterRelevant:
+			content = filter(lambda x : word_is_relevant(x[0]), content)
+		if not limit == False:
+			content = content[:limit]
+	words = dict()
+	for (word, vector) in content:
+		words[word.lower()] = vector
+	return words
+
+if __name__ == '__main__':
+	data = load_vectors("../data/wordvectors/vectors.cw").items()
+	f = open("../data/wordvectors/vectors.relevant.cw", 'w')
+	f.write("0 50")
+	for line in data:
+		f.write(line[0])
+		f.write("".join([ " " + x for x in line[1]])+"\n")
+	f.close()
+
+
+
