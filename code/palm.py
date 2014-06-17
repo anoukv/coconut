8349f48163e4e655473cc9f6dceacb586293bbba
anoukv <anouk.visser@student.uva.nl>
2014-06-17 11:23:05 +0200

    started working on palm

--- code/palm.py
+++ code/palm.py
@@ -1,13 +1,28 @@
-import sys
+import sys, shelve
 
+def chooseMostRelated(word, wordsToChooseFrom, rel):
+	highest = None
+	bestword = None
+	wordRelatednessVec = rel[word]
+	wordsToChooseFrom = filter(lambda x: x in wordRelatednessVec, wordsToChooseFrom)
+	# wordsToChooseFrom = wordsToChooseFrom.remove(word)
+	for candidate in wordsToChooseFrom:
+		relatedness= wordRelatednessVec[candidate]
+		if relatedness > highest and candidate != word:
+			highest = relatedness
+			bestword = candidate
+	return bestword
+
+	return
 def read_file(filename):
 	f = open(filename, 'r')
  	inpt = f.readline().replace("\n", "").split(" ")
  	f.close()
  	return inpt
 
-def getCocMatrix(inpt, skipsize):
-	
+def showExpandedSentences(inpt, rel, skipsize):
+	relevanceVoc = rel.keys()
+	print relevanceVoc
 	queueSize = skipsize * 2 + 1
 	queueMid = skipsize + 1
 
@@ -23,19 +38,31 @@ def getCocMatrix(inpt, skipsize):
 		if queueIsReady(queue):
 			mid = queue[queueMid]
 			if mid == "apple":
+				for word in queue:
+					if word in relevanceVoc:
+						print word
+						print sorted(rel[word].items(), key = lambda x: x[1], reverse=True)[0:3]
 				print len(queue), queue
+				print
 	return 0
 
 if __name__ == "__main__":
 	
 	print "Welcome to PALM!"
 	
-	if len(sys.argv) < 2:
-		print "USAGE: python palm.py <TEXT FILE>"
+	if len(sys.argv) < 3:
+		print "USAGE: python palm.py <TEXT FILE> <PATH TO COC>"
 		sys.exit()
 	textfile = sys.argv[1]
-	
-	inpt = read_file(textfile)
-	getCocMatrix(inpt, 5)
+	relFile = sys.argv[2] + "_rel"
+	rel = shelve.open(relFile)
+	print chooseMostRelated('apple',['mac', 'itools,', 'met', 'varying', 'degrees', 'success', 'apple', 'pomaceous', 'fruit', 'apple', 'tree,'], rel)
+	# inpt = read_file(textfile)
+	# showExpandedSentences(inpt, rel, 5)
 
-	# go over text, get relatedness things
+	# go over text
+	# extract sentence with window
+	# expand sentence with related words (make number of expansions a parameter)
+	# create a vector over the different clusters
+	# find a label for the vector (function chooseMostRelated)
+	# collect labelled training data
