c9867a56d566856fec28cbbf7bdedd55cce299ab
RemideZ <remi.de.z@gmail.com>
2014-06-20 15:20:29 +0200

    clusters

--- code/agg_answering.py
+++ code/agg_answering.py
@@ -1,6 +1,5 @@
-from Word import Word
-from utils import load_task, spearman
-from fast_utils import get_coc
+from utils import load_task, Word
+from fast_utils import get_coc, load_vectors, spearman, cosine_similarity
 
 import shelve
 import sys
@@ -13,14 +12,13 @@ def dic_similarity(large, large_keys, small):
 	return total
 
 def relatedness(context1, context2, cocs, key_sets):
-	# context1 = filter(lambda x : not x == "" and x.relevant(), [ Word(w) for w in context1.lower().split(" ") ])
-	# context1 = [ w.lemma() for w in context1 ]
-	# context2 = filter(lambda x : x.relevant(), [ Word(w) for w in context2.lower().split(" ") ])
-	# context2 = [ w.lemma() for w in context2 ]
+	context1 = filter(lambda x : not x == "" and x.relevant(), [ Word(w) for w in context1.lower().split(" ") ])
+	context1 = [ w.lemma() for w in context1 ]
+	context2 = filter(lambda x : x.relevant(), [ Word(w) for w in context2.lower().split(" ") ])
+	context2 = [ w.lemma() for w in context2 ]
 
-
-	context1 = get_coc(context1.split(" "))
-	context2 = get_coc(context2.split(" "))
+	context1 = get_coc(context1)
+	context2 = get_coc(context2)
 
 	total = 0.0
 	for i in xrange(len(key_sets)):
@@ -30,14 +28,22 @@ def relatedness(context1, context2, cocs, key_sets):
 	total /= len(key_sets)
 	return total
 
+def vector_similarity(cs, w1, w2, vectors):
+	if w1 in vectors and w2 in vectors:
+		return cosine_similarity(vectors[w1], vectors[w2])
+	else:
+		return cs
+
 if __name__ == '__main__':
-	if not len(sys.argv) == 3:
+	if not len(sys.argv) == 4:
 		print "USAGE: python agg_answering.py <PATH TO TASK> <PATH TO cluster_descriptors>"
 		sys.exit()
 
 	print "Loading stuf..."
 	taskFilename = sys.argv[1]
 	filename = sys.argv[2] # "../../../cluster_descriptors/enwiki8.clust-desc.shelve"
+	vectorsFilename = sys.argv[3]
+	vectors = load_vectors(vectorsFilename)
 
 	d = shelve.open(filename)
 	key_sets = []
@@ -57,14 +63,20 @@ if __name__ == '__main__':
 	print "Answering", len(task), "questions..."
 
 	for i in xrange(len(questions)):
-		if i % 100 == 0:
-			print "\tIteration", i
+		if i % 100 == 0 and not i == 0:
+			print "\tIteration", i, ": ", spearman(methodsRating, humanRating)
 
 		question = questions[i]
+
+		word1 = Word(question['word1']).lemma()
+		word2 = Word(question['word2']).lemma()
 		context1 = question['context1'] + " " + question['word1'] + " " + question['word1']
 		context2 = question['context2'] + " " + question['word2'] + " " + question['word2']
 
-		methodsRating.append(relatedness(context1, context2, newD, key_sets))
+		r_s = relatedness(context1, context2, newD, key_sets)
+		v_s = vector_similarity(r_s, word1, word2, vectors)
+		score = r_s * v_s**2
+		methodsRating.append( score )
 		humanRating.append(question['rating'])
 
  	print 
