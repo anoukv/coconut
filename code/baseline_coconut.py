fe4d028f33da15f8056453ba6b58f97b62510ab2
RemideZ <remi.de.z@gmail.com>
2014-06-23 12:24:05 +0200

    working

--- code/baseline_coconut.py
+++ code/baseline_coconut.py
@@ -22,7 +22,7 @@ def getCorrectSense(context, sense1, sense2):
 if __name__ == "__main__":
 	print "Baseline with wordvectors"
 	
-	if len(sys.argv) < 3:
+	if len(sys.argv) < 4:
 		print "USAGE: python baselline_word2vec.py <PATH TO TASK> <PATH TO COC> <PATH TO VECTORS>"
 		sys.exit()
 	
@@ -32,6 +32,7 @@ if __name__ == "__main__":
 	taskFilename = sys.argv[1]
 	cocFilename = sys.argv[2]
 	vectorsFilename = sys.argv[3]
+	questionStart = int(sys.argv[4])
 
 	rel = shelve.open(cocFilename + 'rel')
 	voc = shelve.open(cocFilename + 'voc')
@@ -42,45 +43,53 @@ if __name__ == "__main__":
 
 	vectors = load_vectors(vectorsFilename)
 
+	resultsSelve = shelve.open("results.shelve")
+
 	methodsRating = []
 	humanRating = []
 	cache = dict()
 	
-	questions = task.values()
+	questions = [ y[1] for y in sorted(task.items(), key=lambda x : x[0]) ]
 
 	print "\tLoaded in", int(time() - start + 0.5), "seconds."
 	print "Clustering..."
-	for i in xrange(len(questions)):
-		question = questions[i]
+	for i in xrange(questionStart, len(questions)):
+		if str(i) not in resultsSelve:
+			question = questions[i]
 
-		word1 = Word(question['word1']).lemma()
-		word2 = Word(question['word2']).lemma()
-		
-		# only proceed if both words are known
-		if word1 in vocabulary and word2 in vocabulary:
-			print "\tIteration:", i
+			word1 = Word(question['word1']).lemma()
+			word2 = Word(question['word2']).lemma()
+			
+			# only proceed if both words are known
+			if word1 in vocabulary and word2 in vocabulary:
+				print "\tIteration:", i
 
-			if word1 not in cache:
-				cache[word1] = makeNewCOCS(word1, rel)
-			if word2 not in cache:
-				cache[word2] = makeNewCOCS(word2, rel)
+				if word1 not in cache:
+					cache[word1] = makeNewCOCS(word1, rel)
+				if word2 not in cache:
+					cache[word2] = makeNewCOCS(word2, rel)
 
-			word1Dic = cache[word1]
-			word2Dic = cache[word2]
-			context1 = [ Word(x).lemma() for x in question['context1'].lower().split(' ') ]
-			context2 = [ Word(x).lemma() for x in question['context2'].lower().split(' ') ]
-		
-			senseWord1 = getCorrectSense(context1, word1Dic[0], word1Dic[1])
-			senseWord2 = getCorrectSense(context2, word2Dic[0], word2Dic[1])
+				word1Dic = cache[word1]
+				word2Dic = cache[word2]
+				context1 = [ Word(x).lemma() for x in question['context1'].lower().split(' ') ]
+				context2 = [ Word(x).lemma() for x in question['context2'].lower().split(' ') ]
 			
- 			wordvec1 = getAverageWordRep(senseWord1, vectors)
- 			wordvec2 = getAverageWordRep(senseWord2, vectors)
-			
-			humanRating.append(question['rating'])
-			methodsRating.append(cosine_similarity(wordvec1, wordvec2))
-			
-	 		if len(methodsRating) > 2:
-	 			print "\t\tScore:", spearman(methodsRating, humanRating)
+				senseWord1 = getCorrectSense(context1, word1Dic[0], word1Dic[1])
+				senseWord2 = getCorrectSense(context2, word2Dic[0], word2Dic[1])
+				
+	 			wordvec1 = getAverageWordRep(senseWord1, vectors)
+	 			wordvec2 = getAverageWordRep(senseWord2, vectors)
+				
+				rh = question['rating']
+				rc = cosine_similarity(wordvec1, wordvec2)
+				methodsRating.append(rc)
+				humanRating.append(rh)
+				
+		 		if len(methodsRating) > 2:
+		 			print "\t\tScore:", spearman(methodsRating, humanRating)
+		 	else:
+		 		(rh,rc) = (999,999)
+		 	resultsSelve[str(i)] = (rh,rc)
 
 	stop = time()
 	print "Done in", int(stop - start + 0.5), "seconds."
